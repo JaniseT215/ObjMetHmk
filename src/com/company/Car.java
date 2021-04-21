0ca0b0688441bca9ed2083da4667810519c8fa33
JaniseT215 <janisethorington@gmail.com>
2021-04-21 16:13:49 -0400

    Initial commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Car {
+
+    public static void main(String[] args) {
+        Car myCar = newCar("Move");
+
+
+        int distance = 1;
+        {
+
+
+            if (distance > 0) ;
+            {
+                System.out.println("Moving");
+
+            }
+
+        }
+    }
+
+
+    private static Car newCar(String move) {
+        return null;
+    }
+}
+
+
+
+
+
